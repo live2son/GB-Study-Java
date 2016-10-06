77916cd44ec620994919987bf9cd7bb271e304f0
Сергей Я <yakovlev-sev@ural.rt.ru>
2016-10-06 16:15:59 +0500

    HW 2 - to start.

---
+++ HomeWork_2.java
@@ -0,0 +1,12 @@
+/*
+ *  Авторо программы HomeWork_2.java, Son. Дата создания: 10.10.2016
+ *  TODO: +Finished home work.
+ */
+
+class HomeWork_1 {
+    public static void main (String[] args) {
+
+    }
+
+
+}
